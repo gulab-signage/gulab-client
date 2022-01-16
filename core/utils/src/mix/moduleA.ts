fb618d393d00d1190038313bd5f17220f645b360
Mitko Miloshev <miloshev.mitko@gmail.com>
2022-01-16 13:50:22 +0800

    fix: fix precommit configuration

--- core/utils/src/mix/moduleA.ts
+++
@@ -1,9 +0,0 @@
-export default function moduleA() {
-  const variableA = 'mitko';
-  const myObj = {
-    name: 'mitko',
-    age: 35,
-    environment: 'just need to go far',
-    anotherOne: 'did we pass the limit aidds?',
-  };
-}
