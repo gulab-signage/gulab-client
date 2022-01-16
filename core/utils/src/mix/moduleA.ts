b5ee395fa494f12e22a3be927ca3041c5f51698a
Mitko Miloshev <miloshev.mitko@gmail.com>
2022-01-16 13:33:16 +0800

    test: test precommit hook

--- core/utils/src/mix/moduleA.ts
+++ core/utils/src/mix/moduleA.ts
@@ -1,4 +1,4 @@
 export default function moduleA() {
   const variableA = 'mitko';
-  const myObj = { name: 'mitko', age: 35, environment: 'just need to go far', anotherOne: 'did we pass the limit?' };
+  const myObj = { name: 'mitko', age: 35, environment: 'just need to go far', anotherOne: 'did we pass the limit aids?' };
 }
